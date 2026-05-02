de4e0e578fffd95842a79511d66c4c7522f1fc33
piyushpanwar0006-web <piyushpanwar0006@gmail.com>
2026-05-03 01:15:37 +0530

    feat: add about page with platform mission and feature overview

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -54,7 +54,7 @@ export default function AboutPage() {
           </div>
         </section>
 
-        {/* WHY UNINOTE SECTION */}
+        {/* WHY UNINOTES SECTION */}
         <section className="py-20">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="text-center mb-16">
